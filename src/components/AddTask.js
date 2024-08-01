f7aa6063fd64d5afaa96687208f77c9823b567ab
Jayantm96 <134455119+Jayantm96@users.noreply.github.com>
2024-07-31 22:49:34 -0400

    All good

---
+++ src/components/AddTask.js
@@ -0,0 +1,47 @@
+
+
+const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
+
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (task.id) {
+            const date = new Date();
+            const updateTask = tasklist.map((todo) => (
+                todo.id === task.id ? { id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` } : todo
+            ));
+            setTasklist(updateTask);
+            setTask({});
+
+        } else {
+            const date = new Date();
+            const newTask = { id: date.getTime(), name: e.target.task.value, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` }
+
+            setTasklist([...tasklist, newTask]);
+            
+            setTask({});
+
+        };
+
+
+
+    }
+
+
+
+
+
+    return (
+        <section className="addTask">
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="task" autoComplete="off" value={task.name || ""} placeholder="add task" maxLength="25" onChange={e => setTask({ ...task, name: e.target.value })} />
+                <button type="submit">{task.id ? "Update": "Add"}</button>
+            </form>
+        </section>
+    )
+}
+
+export default AddTask
